fee8a950869f75697762402fd600a62418494725
Alexey Pisanko <apis@users.noreply.github.com>
2022-09-09 16:39:52 -0400

    Updated configs for hub-leaf testing

--- tools/build.ps1
+++ tools/build.ps1
@@ -1,3 +1,8 @@
 go build -v .\cmd\producer
+copy .\cmd\producer\producer_config .
+
 go build -v .\cmd\server
+copy .\cmd\server\server_config .
+
 go build -v .\cmd\consumer
+copy .\cmd\consumer\consumer_config .
