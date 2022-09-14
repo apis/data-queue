bb759ce992cfcd0e07821b0ce7e105e1b08e9eba
Alexey Pisanko <apis@users.noreply.github.com>
2022-09-14 10:12:04 -0400

    Updated configuration for server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/beeker1121/goque"
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"os"
 	"os/signal"
@@ -15,34 +16,54 @@ import (
 func main() {
 	log.Info("Starting up Data Stream Service")
 
+	const natsNameDefault = "NATS Queue Service"
+	const storagePathDefault = "./.queue"
+	const natsSubjectPrefixDefault = "leaf.data-stream"
+
 	viper.SetDefault("natsUrl", "nats://leaf_user:leaf_user@127.0.0.1:34111")
-	viper.SetDefault("storagePath", "./.queue")
-	viper.SetDefault("natsProducerPutSubject", "leaf.data-stream.producer.put")
-	viper.SetDefault("natsConsumerGetSubject", "leaf.data-stream.consumer.get")
-	viper.SetDefault("natsConsumerAckSubject", "leaf.data-stream.consumer.ack")
-	viper.SetDefault("natsConsumerAnnSubject", "leaf.data-stream.consumer.ann")
+	viper.SetDefault("natsName", natsNameDefault)
+	viper.SetDefault("storagePath", storagePathDefault)
+	viper.SetDefault("natsSubjectPrefix", natsSubjectPrefixDefault)
+	viper.SetDefault("natsProducerPutSubjectSuffix", "producer.put")
+	viper.SetDefault("natsConsumerGetSubjectSuffix", "consumer.get")
+	viper.SetDefault("natsConsumerAckSubjectSuffix", "consumer.ack")
+	viper.SetDefault("natsConsumerAnnSubjectSuffix", "consumer.ann")
 	viper.SetConfigName("server_config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath("./")
+
 	if err := viper.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
 			log.Fatalf("fatal error config file: %s", fmt.Errorf("%w", err))
 		}
 	}
+
+	pflag.String("natsName", natsNameDefault, "NATS connection name")
+	pflag.String("storagePath", storagePathDefault, "Storage path directory")
+	pflag.String("natsSubjectPrefix", natsSubjectPrefixDefault, "Storage path directory")
+	pflag.Parse()
+	err := viper.BindPFlags(pflag.CommandLine)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	natsUrl := viper.GetString("natsUrl")
+	natsName := viper.GetString("natsName")
 	storagePath := viper.GetString("storagePath")
-	natsProducerPutSubjectPrefix := viper.GetString("natsProducerPutSubject") + "."
+	natsSubjectPrefix := viper.GetString("natsSubjectPrefix") + "."
+	natsProducerPutSubjectPrefix := natsSubjectPrefix + viper.GetString("natsProducerPutSubjectSuffix") + "."
 	natsProducerPutSubject := natsProducerPutSubjectPrefix + "*"
-	natsConsumerGetSubjectPrefix := viper.GetString("natsConsumerGetSubject") + "."
+	natsConsumerGetSubjectPrefix := natsSubjectPrefix + viper.GetString("natsConsumerGetSubjectSuffix") + "."
 	natsConsumerGetSubject := natsConsumerGetSubjectPrefix + "*"
-	natsConsumerAckSubjectPrefix := viper.GetString("natsConsumerAckSubject") + "."
+	natsConsumerAckSubjectPrefix := natsSubjectPrefix + viper.GetString("natsConsumerAckSubjectSuffix") + "."
 	natsConsumerAckSubject := natsConsumerAckSubjectPrefix + "*"
-	natsConsumerAnnSubjectPrefix := viper.GetString("natsConsumerAnnSubject") + "."
+	natsConsumerAnnSubjectPrefix := natsSubjectPrefix + viper.GetString("natsConsumerAnnSubjectSuffix") + "."
 
 	log.Info("Opening Persisted Queue")
 	queue, err := goque.OpenPrefixQueue(storagePath)
-
-	log.Infof("Queue size %d", queue.Length())
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	defer func() {
 		log.Info("Closing Persisted Queue")
@@ -52,8 +73,24 @@ func main() {
 		}
 	}()
 
+	queueSize := queue.Length()
+	log.Infof("Queue size %d", queueSize)
+	if queueSize == 0 {
+		log.Info("Deleting Queue")
+		err = queue.Drop()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Info("Reopening Persisted Queue")
+		queue, err = goque.OpenPrefixQueue(storagePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
 	log.Infof("Connecting to NATS '%s'", natsUrl)
-	natsConnection, err := common.ConnectToNats(natsUrl, "Data Stream Service Connection")
+	natsConnection, err := common.ConnectToNats(natsUrl, natsName)
 	if err != nil {
 		log.Fatal(err)
 	}
